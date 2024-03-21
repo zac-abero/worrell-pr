176ce0b3dea7e53d3441a3605d1a4c75d9325ac9
zacabero <zac.abero@du.edu>
2024-03-20 20:45:01 -0600

    data_fetch updates plus+notes in excel

--- Tecan_data_exp_handling/data_fetch.py
+++ Tecan_data_exp_handling/data_fetch.py
@@ -6,6 +6,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from win32com.client import dynamic
 
+# Function to locate a cell with a given value in the sheet
+
+def find_cell(value, range, sheet_number):
+    for cell in wb.sheets[sheet_number].range(range)[1:]:
+       # print(cell.value)
+        if cell.value == value:
+            break
+    return cell
+
 # Fetching the active workbook (ideally the one booted by the Tecan i-control software)
 
 wb = xw.books.active
@@ -15,7 +24,10 @@ wb = xw.books.active
 print(wb.sheets[0].range("A1").value)
 
 # Selecting the first column and printing every value in it
-for cell in wb.sheets[0].range('A:A')[1:]:
-    print(cell.value)
-    if cell.value == "End Time:":
-        break
+# for cell in wb.sheets[0].range('A:A')[1:]:
+#     print(cell.value)
+#     if cell.value == "End Time:":
+#         break
+
+print(find_cell("Mean", 'B:B', 0))
+
