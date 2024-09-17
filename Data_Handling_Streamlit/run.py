144e1930753ad5f91d6886bb49f180e7a265c663
Your Name <you@example.com>
2024-09-17 17:18:19 -0600

    adding some functionality changes to streamlit dashboard

--- Data_Handling_Streamlit/run.py
+++ Data_Handling_Streamlit/run.py
@@ -5,3 +5,21 @@ import streamlit_main
 
 import os 
 os.system('streamlit run c:/Users/oppos/Desktop/worrell-pr/Data_Handling_Streamlit/streamlit_main.py')
+
+'''
+# Images
+col1, col2 = st.columns(2, vertical_alignment='bottom')
+# Plotting imagery
+theo_img = os.path.abspath('Data_Handling_Streamlit/images/theo.png')
+zac_img = os.path.abspath('Data_Handling_Streamlit/images/zacbike.png')
+# Open the image from the specified path
+theopng = Image.open(theo_img)
+zacpng = Image.open(zac_img)
+
+# adding image
+
+with col1:
+    st.image(theopng, caption='Theo exhausted after 35 miles and 5kft elevation', use_column_width=True)
+with col2:
+    st.image(zacpng, caption='Zac lifting bike after 35 miles and 5kft elevation', use_column_width=True)
+'''
--- Data_Handling_Streamlit/streamlit_main.py
+++ Data_Handling_Streamlit/streamlit_main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import process_data
 import matplotlib.pyplot as plt
+import os
+from PIL import Image
 
 # Locals
 excel_fname = None
@@ -26,19 +28,40 @@ def display_data():
 def display_graph():
     if data:
         df = pd.read_csv(data)
-        fig, ax = plt.subplots()
-        df.plot(ax=ax)
-        st.pyplot(fig)
+        df = df[["Temperature", "Mean"]].apply(pd.to_numeric, errors = 'coerce') # converts to numeric, leaves "INVALIDs" as NaN
+        #cell_data = df.loc[df['Cell'] == 'A3']
+        st.line_chart(df, x="Temperature", y="Mean", x_label="Temperature", y_label="Mean")
     else:
-        st.error("Please upload a CSV data file.")
+        st.error("Please upload a merged CSV data file.")
 
-st.write("Hello, welcome to the data processing app for the Worrell Lab Plate-Reader Project!")
+# Begin App Layout
+
+# Header
+
+st.header("Hello! welcome to the data processing app for the Worrell Lab Plate-Reader Project!")
+
+# Sidebar
+st.sidebar.radio('Pages', options=['Merge Data', 'Data Analysis', 'Charting'])
+
+# Logo
+# Plotting imagery
+logo_file = os.path.abspath('Data_Handling_Streamlit/images/worrell-lab.png')
+slogo_file = os.path.abspath('Data_Handling_Streamlit/images/worrell-lab-small.png')
+
+# Open the image from the specified path
+logo = Image.open(logo_file)
+logo_small = Image.open(logo_file)
+
+# adding image
+st.logo(logo, link='https://www.bradyworrell.com/',icon_image=logo_small)
+
+# File Upload
 
 excel_fname = st.file_uploader("Excel file", type=['xlsx', 'xls'])
 csv_fname = st.file_uploader("CSV file", type=['csv'])
-data = st.file_uploader("CSV Data file", type=['csv'])
+data = st.file_uploader("Merged CSV Data file", type=['csv'])
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3, vertical_alignment="bottom")
 with col1:
     st.button("Merge Data", on_click=merge_data_button)
     if merged_csv:
@@ -46,8 +69,9 @@ with col1:
 
 with col2:
     st.button("Display Data", on_click=display_data)
-    
-st.button("Display Graph", on_click=display_graph)
+with col3:
+    st.button("Display Graph", on_click=display_graph)
+
 
 
 # TODO: Alter the process_data.py code to merge files on time data - separate that and the cell analysis data so that they can be functional for graphing and manipulation of data
